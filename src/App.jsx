e703f957bc52e412f137d756f1e5b1b912da23fb
Johannes Natunen <johannesnatunen@gmail.com>
2023-04-27 12:33:01 +0300

    add link

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import "./App.css"
-import { TodoInput } from "./components/TodoInput"
-import { TodoList } from "./components/TodoList"
+import "./App.css";
+import { TodoInput } from "./components/TodoInput";
+import { TodoList } from "./components/TodoList";
 
 function App() {
-  const [todo, setTodo] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
   const addTodo = () => {
-    if(todo !== "") {
-      setTodos([...todos, todo])
-      setTodo("")
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
     }
-  }
+  };
 
   const deleteTodo = (text) => {
     const newTodos = todos.filter((todo) => {
-      return todo !== text
-    })
-    setTodos(newTodos)
-  }
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
       <h1>React todo app</h1>
       <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
       <TodoList list={todos} remove={deleteTodo} />
+      <a href="https://thefirma.fi"> Hmm, what's this? </a>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
